eea9149d41ea8587be5f73574200d3f77050bf70
Philippe Leybaert <pleybaert@agileassets.com>
2023-11-19 15:06:31 -0700

    Add project files.

---
+++ Maui-CollectionView/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Maui_CollectionView
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            listview.ItemsSource = Enumerable.Range(0,200000).Select(i => i.ToString()).ToList();
+        }
+    }
+
+}
